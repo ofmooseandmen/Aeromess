97940a89ac401e8a922ec2eb9aede0bd62f433cd
ofmooseandmen <ofmooseandmen@yahoo.com>
2017-09-22 15:51:46 +1000

    Cloud Amount tests.

--- test/Data/Wmo/AerodromeReportParsingSpec.hs
+++ test/Data/Wmo/AerodromeReportParsingSpec.hs
@@ -25,7 +25,7 @@ spec =
                 "ESMS"
                 (13, 12, 50)
                 [withWindDirection 180, withWindSpeed 16 Nothing KT, withPrevailingVisibility 9999]
-        it "parses a METAR with wind gust" $
+        it "parses a METAR with wind gust and cloud amount" $
             parse "METAR ESMS 131250Z 18016G20KMH 9999 BKN008 12/12 Q0988" `shouldBe`
             metar
                 "ESMS"
@@ -33,9 +33,10 @@ spec =
                 [ withWindDirection 180
                 , withWindSpeed 16 (Just 20) KMH
                 , withPrevailingVisibility 9999
+                , withCloudAmount Broken (Just 8) Nothing
                 ]
         it "parses a METAR with variable wind" $
-            parse "METAR ESMS 131250Z VRB16KT 9999 BKN008 12/12 Q0988" `shouldBe`
+            parse "METAR ESMS 131250Z VRB16KT 9999 12/12 Q0988" `shouldBe`
             metar "ESMS" (13, 12, 50) [withWindSpeed 16 Nothing KT, withPrevailingVisibility 9999]
         it "parses a corrected METAR" $
             parse "METAR COR LFPG 152330Z 25003KT CAVOK 10/09 Q1012 NOSIG" `shouldBe`
@@ -85,7 +86,7 @@ spec =
                 ]
         it "parses a METAR with prevailing visibility and Runway Visual Range (RVR)" $
             parse
-                "METAR LBBG 041600Z 12012MPS 090V150 1400 R04/P1500N R11/1200D R22/M0050U R27/0040 BKN022 OVC050 M04/M07 Q1020 NOSIG 8849//91" `shouldBe`
+                "METAR LBBG 041600Z 12012MPS 090V150 1400 R04/P1500N R11/1200D R22/M0050U R27/0040 M04/M07 Q1020 NOSIG 8849//91" `shouldBe`
             metar
                 "LBBG"
                 (4, 16, 0)
@@ -119,4 +120,16 @@ spec =
                 , withWeather Nothing (Just Patches) [Fog]
                 , withWeather (Just HeavyWeather) Nothing [Mist]
                 , withWeather (Just InVicinityWeather) Nothing [Snow]
+                ]
+        it "parses a SPECI with heavy rain and few/overcast cloud amounts" $
+            parse "SPECI ESMS 131250Z 18016KT 9999 +RA FEW022CB OVC050 12/12 Q0988" `shouldBe`
+            speci
+                "ESMS"
+                (13, 12, 50)
+                [ withWindDirection 180
+                , withWindSpeed 16 Nothing KT
+                , withPrevailingVisibility 9999
+                , withWeather (Just HeavyWeather) Nothing [Rain]
+                , withCloudAmount Few (Just 22) (Just Cumulonimbus)
+                , withCloudAmount Overcast (Just 50) Nothing
                 ]
--- test/Data/Wmo/FaaAerodromeReportParsingSpec.hs
+++ test/Data/Wmo/FaaAerodromeReportParsingSpec.hs
@@ -12,7 +12,7 @@ spec =
     describe "FAA METAR paser" $ do
         it "parses a basic METAR" $
             parse
-                "METAR KJFK 151151Z 25004KT 10SM FEW150 FEW250 22/18 A3003 RMK AO2 SLP169 T02170183 10217 20189 51020" `shouldBe`
+                "METAR KJFK 151151Z 25004KT 10SM 22/18 A3003" `shouldBe`
             metar
                 "KJFK"
                 (15, 11, 51)
@@ -22,7 +22,7 @@ spec =
                 ]
         it "parses a METAR with prevailing visibility expressed in mile and fraction" $
             parse
-                "METAR KJFK 151151Z 25004KT 10 1/4SM FEW150 FEW250 22/18 A3003 RMK AO2 SLP169 T02170183 10217 20189 51020" `shouldBe`
+                "METAR KJFK 151151Z 25004KT 10 1/4SM 22/18 A3003" `shouldBe`
             metar
                 "KJFK"
                 (15, 11, 51)
@@ -32,7 +32,7 @@ spec =
                 ]
         it "parses a METAR with prevailing visibility expressed in fraction only" $
             parse
-                "METAR KJFK 151151Z 25004KT 1/4SM FEW150 FEW250 22/18 A3003 RMK AO2 SLP169 T02170183 10217 20189 51020" `shouldBe`
+                "METAR KJFK 151151Z 25004KT 1/4SM 22/18 A3003" `shouldBe`
             metar
                 "KJFK"
                 (15, 11, 51)
